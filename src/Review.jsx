af4c32aafb68b5f0eccb4d5cd5c990a62923c348
Abdul Rehamn <92205472+likeam@users.noreply.github.com>
2024-04-28 18:22:17 +0500

    Building ui

--- src/Review.jsx
+++ src/Review.jsx
@@ -1,21 +1,37 @@
-import { FaChevronRight,  FaChevronDown, FaChevronLeft, FaQuoteRight } from "react-icons/fa"
-import people from "./data"
-import { useState } from "react"
+import {
+  FaChevronRight,
+  FaChevronDown,
+  FaChevronLeft,
+  FaQuoteRight,
+} from "react-icons/fa";
+import people from "./data";
+import { useState } from "react";
 
 const Review = () => {
-
-    const [index, setIndex] = useState(0)
-    const {name, job, image, text} = people[index]
+  const [index, setIndex] = useState(0);
+  const { name, job, image, text } = people[index];
 
   return (
     <article className="review">
-        <div className="img-container">
-            <img src={image} alt={name} className="person-img" />
-            <span className="quote-icon">
-                <FaQuoteRight />
-            </span>
-        </div>
+      <div className="img-container">
+        <img src={image} alt={name} className="person-img" />
+        <span className="quote-icon">
+          <FaQuoteRight />
+        </span>
+      </div>
+      <h4 className="author">{name}</h4>
+      <p className="job">{job}</p>
+      <p className="info">{text}</p>
+      <div className="button-container">
+        <button className="prev-btn">
+          <FaChevronLeft />
+        </button>
+        <button className="next-btn">
+          <FaChevronRight />
+        </button>
+      </div>
+      <button className="random-btn">Surpise Me</button>
     </article>
-  )
-}
-export default Review
+  );
+};
+export default Review;
