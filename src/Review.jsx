abd61101547eacadc2545f3d21146fb4067fcfb3
Abdul Rehamn <92205472+likeam@users.noreply.github.com>
2024-04-28 18:48:37 +0500

    Building Next & Previous Button

--- src/Review.jsx
+++ src/Review.jsx
@@ -7,9 +7,35 @@ import {
 import people from "./data";
 import { useState } from "react";
 
+
 const Review = () => {
   const [index, setIndex] = useState(0);
   const { name, job, image, text } = people[index];
+
+  const checkNumber = (number) => {
+    if(number >people.length -1){
+      return 0;
+    }
+    if(number < 0){
+      return people.length -1;
+    }
+    return number;
+  }
+
+  
+const nextPerson = () => {
+  setIndex((index) => {
+    let newIndex = index + 1;
+    return checkNumber(newIndex);
+  })
+}
+
+const prevPerson = () => {
+  setIndex((index) => {
+    let newIndex = index - 1;
+    return checkNumber(newIndex);
+  })
+}
 
   return (
     <article className="review">
@@ -23,11 +49,11 @@ const Review = () => {
       <p className="job">{job}</p>
       <p className="info">{text}</p>
       <div className="button-container">
-        <button className="prev-btn">
+        <button className="prev-btn" onClick={prevPerson}>
           <FaChevronLeft />
         </button>
-        <button className="next-btn">
-          <FaChevronRight />
+        <button className="next-btn" >
+          <FaChevronRight onClick={nextPerson} />
         </button>
       </div>
       <button className="random-btn">Surpise Me</button>
